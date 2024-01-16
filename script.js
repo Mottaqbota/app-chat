a4dfeae78b5ae28678ee8ea43be6be060eeb1b2d
Motta <125099026+Mottaqbota@users.noreply.github.com>
2024-01-16 15:09:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ var contactsData = [
 	    a: "",
 	},
 ]
+
+{/* 
+
+<div class="chat-card">
+	<div class="box-img">
+		<i class="fa-solid fa-circle user-stats-dot"></i>
+		<img class="user-img" src="https://www.gov.br/cdn/sso-status-bar/src/image/user.png">
+	</div>
+	<div class="box-infos">
+		<div class="box-username">
+			<h1 class="username">Username</h1>
+			<i class="fa-solid fa-circle new-msg-dot"></i>
+		</div>
+		<div class="box-last-msg">
+			<i class="fa-regular fa-circle-dot"></i>
+			<p class="last-msg">Fala mano, quer sair pra toma um açaízão?</p>
+		</div>
+	</div>
+	<div class="box-date">
+		<i class="fa-solid fa-volume-xmark"></i>
+		<p class="date-last-msg">12:31</p>
+	</div>
+</div> 
+
+*/}
